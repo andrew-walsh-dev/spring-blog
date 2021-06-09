6d1e91d51c532c38b157402712427c49b671aca9
Amdrew Walsh <andrew.jordan.walsh@gmail.com>
2021-06-09 11:34:49 -0500

    finished controller exercises

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,22 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloName(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+}
